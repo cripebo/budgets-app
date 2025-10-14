cc2115d275d356f0d8cb4a6a6f18c1024f458b70
Cristian Peris <cristianperisb@gmail.com>
2025-10-14 12:36:53 +0200

    feat(app-init): include item categories in global initialization
    - Injects `ItemCategoriesService` into `AppInitService`
    - Calls `itemsCategories.loadAll()` to preload categories on app startup

--- src/app/core/services/app-init.service.ts
+++ src/app/core/services/app-init.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BudgetsService } from '@features/budgets/budgets.service';
 import { ClientsService } from '@features/clients/clients.service';
+import { ItemCategoriesService } from '@features/items/services/item-categories.service';
 import { ItemsService } from '@features/items/services/items.service';
 import { SettingsService } from '@features/settings/settings.service';
 import { StatesService } from '@features/states/states.service';
@@ -11,6 +12,7 @@ export class AppInitService {
     private budgets: BudgetsService,
     private clients: ClientsService,
     private items: ItemsService,
+    private itemsCategories: ItemCategoriesService,
     private settings: SettingsService,
     private states: StatesService,
   ) {}
@@ -19,6 +21,7 @@ export class AppInitService {
     this.budgets.loadAll();
     this.clients.loadAll();
     this.items.loadAll();
+    this.itemsCategories.loadAll();
     this.settings.loadAll();
     this.states.loadAll();
   }
